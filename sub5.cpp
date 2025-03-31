38655d4760eba9c6e1f8192ac8d08552ab6c7fb9
coo1cattt <163154183+coo1cattt@users.noreply.github.com>
2025-03-31 15:44:56 +0900

    Create sub5.cpp

---
+++ sub5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// opp 5 
+
+int main(){
+    int a;
+    cout << "정수 입력 : ";
+    cin >> a;  
+    cout << "8진수 : " << showbase << oct << a << endl; 
+    cout << "16진수 :" << hex << a << endl;
+    return 0;
+}    
+                         
