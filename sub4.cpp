2fde8ee94a48e4d21f0db284a63a1495d25dccea
coo1cattt <163154183+coo1cattt@users.noreply.github.com>
2025-03-31 15:44:28 +0900

    Create sub4.cpp

---
+++ sub4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// opp 4 
+
+int main(){
+    double fd, cd;
+    cout << "화씨온도 입력 : ";
+    cin >> fd;  
+    cd = (5.0/9.0)*(fd-32.0);
+    cout << "섭씨온도는 " << cd << "입니다." << endl;
+
+    return 0;
+}                              
