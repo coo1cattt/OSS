37c2f74b83ce21bb1c1395e49a35f2eaacbb721e
coo1cattt <163154183+coo1cattt@users.noreply.github.com>
2025-04-07 15:10:59 +0900

    Create sub12.cpp

---
+++ sub12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// 5장 q6
+
+long long mypow(int a, int b){
+    long long result = 1;
+    for(int i = 0; i < b; ++i){
+        result *= a;
+    }
+
+    return result;
+}
+
+int main(){
+
+    int a, b;
+    cout << "a와 b를 입력하세요 : ";
+    cin >> a >> b;
+    cout << a << "의 " << b << "승은 " << mypow(a,b) << endl; 
+    
+    return 0;
+}
