56708e1f9406eaea7bce64eb262d8f5cacc1f54c
coo1cattt <163154183+coo1cattt@users.noreply.github.com>
2025-05-09 22:20:10 +0900

    Create oop_sub19.cpp

---
+++ oop_sub19.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// 참조변수 
+
+int main(){
+
+    int x, y;
+    cout << "두 정수 입력 :";
+    cin >> x >> y;
+
+    cout << "x" << x << ",y" << y << endl; // befor
+
+    cout << "x = " << x << ",y = " << y << endl; // after
+
+    return 0;
+
+
+}
